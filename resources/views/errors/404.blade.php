fd271e1b859ff0de007f4801014a04d1b62f4930
Andrew <andrew@meetthemaxwells.com>
2017-06-07 12:28:08 -0700

    Adding 404 page and redirecting registration to it

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,22 +2,18 @@
 
 @section('content')
 
-  <body class="marketing register">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-md-6 col-md-offset-3">
+<body class="marketing register">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-6 col-md-offset-3">
 
-        <div class="alert alert-danger">
-          <h3><i class="fa fa-ban"></i> @lang('auth.not_authorized')</h3>
+				<div class="alert alert-danger">
+					<h3><i class="fa fa-ban"></i> Uh oh! We can't find what you're looking for.</h3>
+				</div>
 
-          @if(isset($exception) && $exception->getMessage())
-            <p>{{ $exception->getMessage() }}</p>
-          @endif
-        </div>
-
-      </div>
-    </div>
-  </div>
-  </body>
+			</div>
+		</div>
+	</div>
+</body>
 
 @endsection
